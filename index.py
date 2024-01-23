7f67030f3d3bd5c9395b912071bd7f7cf0d546ed
Georgy <georgy.polkanov@yandex.ru>
2024-01-24 00:48:06 +0300

    setting TelegramClient

--- index.py
+++ index.py
@@ -1,16 +1,23 @@
 # Подключаем библиотеку telethon
-from telethon import TelegamClient
+from telethon import TelegramClient, events, sync
 
 # Подставляем собственные значения из 'my.telethon.org'
 api_id = 27662919
 api_hash = 'f3065d29712e006e64ea1703d577f9c5'
-bot_token = '27662919:f3065d29712e006e64ea1703d577f9c5'
+#api_token = '27662919:f3065d29712e006e64ea1703d577f9c5'
 
-# если нам нужен явный токен бота, 
-# то нужно вручную вызвать `TelegramClient.start()`.
-bot = TelegamClient('bot', api_id, api_hash).start(bot_token=bot_token)
+client = TelegramClient('bot', api_id, api_hash)
 
-# Тогда можем использовать экземпляр клиента как обычно.
-with bot as bot_client:
-    proxy = ("85312", '127.0.0.1', 4444)
-    TelegamClient('bot', api_id, api_hash, proxy=proxy)
+async def main():
+    me = await client.get_me()
+
+    print(me.stringify())
+
+    username = me.username
+    print(username)
+    print(me.phone)
+
+    async for dialog in client.iter_dialogs():
+        print(dialog.name, 'has ID', dialog.id)
+
+client.start()
