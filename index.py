63d8b341317220743d0e640fabdccc9b583fb3fa
GeorgyHub <georgy.polkanov@yandex.ru>
2024-06-09 21:36:59 +0300

    first step

--- index.py
+++ index.py
@@ -1,23 +1,19 @@
 # Подключаем библиотеку telethon
-from telethon import TelegramClient, events, sync
+from telethon.sync import TelegramClient, events
 
 # Подставляем собственные значения из 'my.telethon.org'
 api_id = 27662919
 api_hash = 'f3065d29712e006e64ea1703d577f9c5'
-#api_token = '27662919:f3065d29712e006e64ea1703d577f9c5'
+api_token = '7268393537:AAE4CvloPV1JqEbW0q6y1n4RHYNfc0rmoZg'
 
-client = TelegramClient('bot', api_id, api_hash)
+bot = TelegramClient('bot', api_id, api_hash).start(bot_token=api_token)
 
-async def main():
-    me = await client.get_me()
+@bot.on(events.NewMessage(pattern='/start'))
+async def start(event):
+    await event.respond('Hello')
 
-    print(me.stringify())
+def main():
+    bot.run_until_disconnected()
 
-    username = me.username
-    print(username)
-    print(me.phone)
-
-    async for dialog in client.iter_dialogs():
-        print(dialog.name, 'has ID', dialog.id)
-
-client.start()
+if __name__ == '__main__':
+    main()
